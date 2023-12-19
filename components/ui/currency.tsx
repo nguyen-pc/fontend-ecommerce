b637442acb8982ca02a904709de19685c0770bc3
nguyen <nguyenthcs430@gmail.com>
2023-12-19 20:30:04 +0700

    second commit

---
+++ components/ui/currency.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+interface CurrencyProps {
+  value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({ value }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  });
+  return <div className="font-semibold">{formatter.format(Number(value))}</div>;
+};
+
+export default Currency;
